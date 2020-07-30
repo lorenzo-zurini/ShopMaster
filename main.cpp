6df767110683ae5877bf53ef4b798bdbf9c0498c
secovlad <vs2u16@soton.ac.uk>
2020-07-30 17:37:03 +0100

    [Edit] Poule Soule

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "mainwindow.h"
+#include <QThread>
 
 #include <QApplication>
 
@@ -8,4 +9,6 @@ int main(int argc, char *argv[])
     MainWindow w;
     w.show();
     return a.exec();
+
+    QThread::sleep(100);
 }
